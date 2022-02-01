cf5fcde8f4afd63cc25ea65dc19b1bf0d4b65341
Sandra <sandraluciagordillo@hotmail.com>
2022-02-01 10:41:35 -0500

    ajustes-Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Navbar() {
+  return <div className='Navbar'>
+<nav className='navbar navbar-expand-lg navbar-light bg-light'>
+  <div className='container-fluid'>
+    <a className='navbar-brand' href="#">Music Site</a>
+    <button className='navbar-toggler' type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class='navbar-toggler-icon'></span>
+    </button>
+    <div className='collapse navbar-collapse' id="navbarNavAltMarkup">
+      <div className='navbar-nav'>
+        <a className='nav-link' href="#">Inicio</a>
+        <a className='nav-link' href="#">Tienda Online</a>
+        <a className='nav-link' href="#">Ofertas</a>
+      </div>
+    </div>
+  </div>
+</nav>
+  </div>;
+}
+
+export default Navbar;
